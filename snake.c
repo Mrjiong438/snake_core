89fd13187856316fa8d4f92ac9d28dac75e8be74
mrjiong <3268839661@qq.com>
2024-11-01 22:30:17 +0800

    version1.0

---
+++ snake.c
@@ -0,0 +1,151 @@
+#include "snake.h"
+
+Position8 head={0};
+Position8 tail={0};
+char map[MAPSIZE][MAPSIZE]={0};
+char hitwall=1;
+int gtime=0;
+char score=0;
+char lastgc=0,gc='N',input=0;
+
+Position8 pointmove(Position8 point){
+	Position8 result={0};
+
+	switch (map[point.x][point.y]){
+		case '^':
+			setpos(&result,point.x,point.y-1);
+			break;
+		case 'v':
+			setpos(&result,point.x,point.y+1);
+			break;
+		case '<':
+			setpos(&result,point.x-1,point.y);
+			break;
+		case '>':
+			setpos(&result,point.x+1,point.y);
+			break;
+	}
+
+	return result;
+}
+
+int change(){
+	Position8 newhead={0};
+	Position8 newtail={0};
+
+	newhead=pointmove(head);
+	newtail=pointmove(tail);
+
+//#ifdef HITWALL 
+	if (hitwall && (newhead.x<0 
+		|| newhead.x>=MAPSIZE
+		|| newhead.y<0
+		|| newhead.y>=MAPSIZE))
+		return 1;
+//#endif
+
+	newhead.x=(newhead.x+MAPSIZE) % MAPSIZE;
+	newhead.y=(newhead.y+MAPSIZE) % MAPSIZE;
+
+	newtail.x=(newtail.x+MAPSIZE) % MAPSIZE;
+	newtail.y=(newtail.y+MAPSIZE) % MAPSIZE;
+
+	switch (map[newhead.x][newhead.y]){
+		case '@':
+			score++;
+			setchar(newhead.x,newhead.y,map[head.x][head.y]);
+			setpos_P(&head,newhead);
+			genfood();
+			break;
+		case '^':
+		case 'v':
+		case '<':
+		case '>':
+			if (newhead.x!=tail.x && newhead.y!=tail.y)
+				return 1;
+			
+			setchar(newhead.x,newhead.y,map[head.x][head.y]);
+			setpos_P(&head,newhead);
+
+			setpos_P(&tail,newtail);
+
+			break;
+		default :
+			setchar(newhead.x,newhead.y,map[head.x][head.y]);
+			setpos_P(&head,newhead);
+
+			setchar(tail.x,tail.y,0);
+			setpos_P(&tail,newtail);
+	}
+
+	if(score==MAPSIZE*MAPSIZE)
+		return 2;
+
+	return 0;
+}
+
+int initHeadPos(unsigned char x,unsigned char y,char ch){
+	setchar(x,y,ch);
+	setpos(&head,x,y);
+	setpos(&tail,x,y);
+}
+int initHeadPos_P(Position8 pos,char ch){
+	setchar(pos.x,pos.y,ch);
+	setpos(&head,pos.x,pos.y);
+	setpos(&tail,pos.x,pos.y);
+}
+
+void setpos(Position8 *obj,char x,char y){
+	obj->x=x;
+	obj->y=y;
+}
+
+void setpos_P(Position8 *obj,Position8 trgit){
+	obj->x=trgit.x;
+	obj->y=trgit.y;
+}
+
+void setchar(char x,char y,char ch){
+	map[x][y] = ch;
+}
+
+void setchar_P(Position8 pos,char ch){
+	map[pos.x][pos.y] = ch;
+}
+
+int	setdirUP(){
+	if (map[head.x][((head.y-1)+MAPSIZE)%MAPSIZE]=='v')
+		return 1;
+	setchar(head.x,head.y,'^');
+	return 0;
+}
+int setdirDOWN(){
+	if (map[head.x][(head.y+1)%MAPSIZE]=='^')
+		return 1;
+	setchar(head.x,head.y,'v');
+	return 0;
+}
+int setdirLEFT(){
+	if (map[((head.x-1)+MAPSIZE)%MAPSIZE][head.y]=='>')
+		return 1;
+	setchar(head.x,head.y,'<');
+	return 0;
+}
+int setdirRIGHT(){
+	if (map[((head.x+1)+MAPSIZE)%MAPSIZE][head.y]=='<')
+		return 1;
+	setchar(head.x,head.y,'>');
+	return 0;
+}
+
+void genfood(){
+	int x=0;
+	int y=0;
+	while(1){
+		x=rand() % MAPSIZE;
+		y=rand() % MAPSIZE;
+		if (map[x][y] == 0)
+			break;
+	}
+	setchar(x,y,'@');
+}
---
+++ snake.h
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAPSIZE 8
+
+#define HITWALL
+
+typedef struct {
+	char x;
+	char y;
+} Position8;
+
+extern char hitwall;
+extern Position8 head;
+extern Position8 tail;
+extern char map[MAPSIZE][MAPSIZE];
+extern int gtime;
+extern unsigned char socre;
+extern char lastgc,gc,input;
+
+#ifdef _WIN32
+	#define clear system("cls");
+#elif __linux__
+	#define clear system("clear");
+#endif
+
+int initHeadPos(unsigned char x,unsigned char y,char ch);
+int initHeadPos_P(Position8 pos,char ch);
+
+void setpos(Position8 *obj,char x,char y);
+void setpos_P(Position8 *obj,Position8 trgit);
+
+void setchar(char x,char y,char ch);
+void setchar_P(Position8 pos,char ch);
+
+void genfood();
+
+int setdirUP();
+int setdirDOWN();
+int setdirLEFT();
+int setdirRIGHT();
+
+Position8 pointmove(Position8 point);
+
+int change();
+
